f95cb33cf0b542a78f1289611c0d8edc7f716743
Malte Franken <exxamalte@users.noreply.github.com>
2019-12-30 12:32:48 +1100

    add unit tests

--- tests/test_geometries.py
+++ tests/test_geometries.py
@@ -14,6 +14,12 @@ class TestGeometries(unittest.TestCase):
         assert point.longitude == 149.2345
         assert repr(point) == "<Point(latitude=-37.1234, longitude=149.2345)>"
 
+    def test_point_equality(self):
+        """Test points."""
+        point1 = Point(10.0, 15.0)
+        point2 = Point(10.0, 15.0)
+        assert point1 == point2
+
     def test_polygon(self):
         """Test polygon."""
         polygon = Polygon([
@@ -28,6 +34,24 @@ class TestGeometries(unittest.TestCase):
         assert polygon.centroid.longitude == 150.32
         assert repr(polygon) == "<Polygon(centroid=" \
                                 "<Point(latitude=-30.32, longitude=150.32)>)>"
+
+    def test_polygon_equality(self):
+        """Test points."""
+        polygon1 = Polygon([
+            Point(30.0, 30.0),
+            Point(30.0, 35.0),
+            Point(35.0, 35.0),
+            Point(35.0, 30.0),
+            Point(30.0, 30.0)
+        ])
+        polygon2 = Polygon([
+            Point(30.0, 30.0),
+            Point(30.0, 35.0),
+            Point(35.0, 35.0),
+            Point(35.0, 30.0),
+            Point(30.0, 30.0)
+        ])
+        assert polygon1 == polygon2
 
     def test_bounding_box_1(self):
         """Test bounding box."""
@@ -64,3 +88,9 @@ class TestGeometries(unittest.TestCase):
         assert bbox.centroid.longitude == 180.0
         assert bbox.is_inside(Point(-2.5, -179.0))
         assert not bbox.is_inside(Point(2.5, -170.0))
+
+    def test_bounding_box_equality(self):
+        """Test points."""
+        bbox1 = BoundingBox(Point(10.0, 10.0), Point(20.0, 20.0))
+        bbox2 = BoundingBox(Point(10.0, 10.0), Point(20.0, 20.0))
+        assert bbox1 == bbox2
