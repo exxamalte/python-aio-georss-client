94af60ca3d05e15c9162a2ac03bef4a3c4abba3b
Malte Franken <exxamalte@users.noreply.github.com>
2023-01-26 14:53:37 +1100

    bump haversine to 2.7.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ URL = "https://github.com/exxamalte/python-aio-georss-client"
 
 REQUIRES = [
     "aiohttp>=3.7.4,<4",
-    "haversine>=1.0.1",
+    "haversine>=2.7.0",
     "xmltodict>=0.12.0",
     "dateparser>=1.1.6",
 ]
