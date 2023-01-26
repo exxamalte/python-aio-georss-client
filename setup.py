5d3be9f3b13c611807e74daa7a12d7a8bc19b726
Malte Franken <exxamalte@users.noreply.github.com>
2023-01-26 15:00:07 +1100

    bump xmltodict to 0.13.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ URL = "https://github.com/exxamalte/python-aio-georss-client"
 REQUIRES = [
     "aiohttp>=3.7.4,<4",
     "haversine>=2.7.0",
-    "xmltodict>=0.12.0",
+    "xmltodict>=0.13.0",
     "dateparser>=1.1.6",
 ]
 
